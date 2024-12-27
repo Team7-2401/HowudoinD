8261aca037224d7d41d4afc3041155ed8b0a9cf6
Ali Ishtay <alijpfu0@gmail.com>
2024-12-27 14:51:19 +0300

    Fixed constants file to account for both simulators.

--- app/config/constants.ts
+++ app/config/constants.ts
@@ -1,1 +1,7 @@
-export const SERVER_URL = 'http://10.0.2.2:8080';
+import { Platform } from 'react-native';
+
+export const SERVER_URL = Platform.select({
+  ios: 'http://localhost:8080',
+  android: 'http://10.0.2.2:8080',
+  default: 'http://10.0.2.2:8080',
+});
